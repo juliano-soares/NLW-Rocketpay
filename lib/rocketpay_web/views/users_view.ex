4d5da47bb0e1fb9c0eda2388c7850839f655c10d
juliano leonardo soares <julianoleonardosoares@hotmail.com>
2021-03-04 23:59:17 -0300

    finished the second class

---
+++ lib/rocketpay_web/views/users_view.ex
@@ -0,0 +1,17 @@
+defmodule RocketpayWeb.UsersView do
+  alias Rocketpay.User
+
+  def render("create.json", %{user: %User{id: id, name: name, nickname: nickname}}) do
+    %{
+      message: "User created",
+      user: %{
+        id: id,
+        name: name,
+        nickname: nickname
+      }
+    }
+  end
+
+  def render("400.json") do
+  end
+end
